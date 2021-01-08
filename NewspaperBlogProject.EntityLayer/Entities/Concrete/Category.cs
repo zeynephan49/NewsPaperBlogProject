e517523b25eb252236877a61b8921f38b78f4d33
zeynep han <zeynephan99@icloud.com>
2021-01-08 18:39:24 +0300

    Add project files.

---
+++ NewspaperBlogProject.EntityLayer/Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewspaperBlogProject.EntityLayer.Entities.Concrete
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+}
